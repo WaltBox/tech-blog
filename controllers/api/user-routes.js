d97fe9f1b2e1b5070547c06f231c15c8e7b100ec
Walt Boxwell <waltboxwell@gmail.com>
2022-07-12 18:55:30 -0500

    Added login/logout

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -96,6 +96,17 @@ router.post('/login', (req,res)=> {
     });
 });
 
+router.post('/logout', (req,res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    }
+    else {
+        res.status(404).end();
+    }
+})
+
 
 // //Create a new user?
 // router.post('/', async (req, res) => {
